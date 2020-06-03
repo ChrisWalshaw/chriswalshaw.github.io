1e1193cbdf94702816644515ea280b5ff8a8d119
ChrisWalshaw <59966821+ChrisWalshaw@users.noreply.github.com>
2020-06-03 06:29:34 +0100

    Update TuneGraphConfig.v2.js

--- js/TuneGraphConfig.v2.js
+++ js/TuneGraphConfig.v2.js
@@ -4,9 +4,29 @@ var DIAGNOSTICS = true;
 window.onload = init;
 
 function init() {
+	var suffix = LINK_TO_LOCAL_JSON_FILES ? '_local' : '';
+	var abcElements = document.getElementsByClassName('divAbcTuneGraphContainer');
+	var nAbcCanvasElements = document.getElementsByClassName('divAbcTuneGraphCanvas').length;
+	var nAbcScoreElements = document.getElementsByClassName('divAbcTuneGraphScore').length;
+	for (var i = 0; i < abcElements.length; ++i) {
+		var abcElement = abcElements[i];
+		var tuneId = abcElement.id;
+		if (nAbcCanvasElements === 0 && nAbcScoreElements === 0) {
+			abcElement.innerHTML = '<table><tr><td>' + abcElement.innerHTML + '</td><td><div id="' + tuneId + '.canvas" class="divAbcTuneGraphCanvas"></div></td><td><div id="' + tuneId + '.score" class="divAbcTuneGraphScore"></div></td></table>';
+		}
+		if (!DIAGNOSTICS) {
+			var tuneCode = window.location.hostname + window.location.pathname + '/' + tuneId;
+			TuneGraph('http://abcnotation.com/getResource/resources/data_/title' + suffix + '.json?a=' + tuneCode, tuneId + '.canvas', tuneId + '.score', false, LINK_TO_LOCAL_JSON_FILES, DIAGNOSTICS);
+		}
+	}
     if (DIAGNOSTICS) {
         var diagnosticsDiv = document.getElementById('tuneGraphDiagnostics');
         diagnosticsDiv.innerHTML = '<p>' + window.location.hostname + window.location.pathname + '</p>';
+        //var containerElements = document.getElementsByClassName('divAbcTuneGraphContainer');
+        //for (var i = 0; i < containerElements.length; ++i) {
+        //    var tuneCode = window.location.hostname + window.location.pathname + '/' + containerElements[i].id;
+        //    containerElements[i].innerHTML = '<p> ' + tuneCode + '</p>';
+        //}
         var canvasElements = document.getElementsByClassName('divTuneGraphCanvas');
         for (var i = 0; i < canvasElements.length; ++i) {
             var tuneCode = window.location.hostname + window.location.pathname + '/' + canvasElements[i].id;
@@ -16,20 +36,6 @@ function init() {
         for (var i = 0; i < scoreElements.length; ++i) {
             var tuneCode = window.location.hostname + window.location.pathname + '/' + scoreElements[i].id;
             scoreElements[i].innerHTML = '<p> ' + tuneCode + '</p>';
-        }
-    } else {
-        var suffix = LINK_TO_LOCAL_JSON_FILES ? '_local' : '';
-        var abcElements = document.getElementsByClassName('divAbcTuneGraphContainer');
-        var nAbcCanvasElements = document.getElementsByClassName('divAbcTuneGraphCanvas').length;
-        var nAbcScoreElements = document.getElementsByClassName('divAbcTuneGraphScore').length;
-        for (var i = 0; i < abcElements.length; ++i) {
-            var abcElement = abcElements[i];
-            var tuneId = abcElement.id;
-            if (nAbcCanvasElements === 0 && nAbcScoreElements === 0) {
-                abcElement.innerHTML = '<table><tr><td>' + abcElement.innerHTML + '</td><td><div id="' + tuneId + '.canvas" class="divAbcTuneGraphCanvas"></div></td><td><div id="' + tuneId + '.score" class="divAbcTuneGraphScore"></div></td></table>';
-            }
-            var tuneCode = window.location.hostname + window.location.pathname + '/' + tuneId;
-            TuneGraph('http://abcnotation.com/getResource/resources/data_/title' + suffix + '.json?a=' + tuneCode, tuneId + '.canvas', tuneId + '.score', false, LINK_TO_LOCAL_JSON_FILES, DIAGNOSTICS);
         }
     }
 }
