ea45d7f2982d8b6b27a99c74ac8ceab394553e63
ChrisWalshaw <59966821+ChrisWalshaw@users.noreply.github.com>
2020-06-03 06:41:31 +0100

    Update TuneGraphConfig.v2.js

--- js/TuneGraphConfig.v2.js
+++ js/TuneGraphConfig.v2.js
@@ -16,9 +16,13 @@ function init() {
 			abcElement.innerHTML = '<table><tr><td>' + abcElement.innerHTML + '</td><td><div id="' + tuneId + '.canvas" class="divAbcTuneGraphCanvas"></div></td><td><div id="' + tuneId + '.score" class="divAbcTuneGraphScore"></div></td></table>';
 			nTablesCreated += 1;
 		}
+		var tuneCode = window.location.hostname + window.location.pathname + '/' + tuneId;
+		var jsonFileName = 'http://abcnotation.com/getResource/resources/data_/title' + suffix + '.json?a=' + tuneCode;
 		if (!DIAGNOSTICS) {
-			var tuneCode = window.location.hostname + window.location.pathname + '/' + tuneId;
-			TuneGraph('http://abcnotation.com/getResource/resources/data_/title' + suffix + '.json?a=' + tuneCode, tuneId + '.canvas', tuneId + '.score', false, LINK_TO_LOCAL_JSON_FILES, DIAGNOSTICS);
+			TuneGraph(jsonFileName, tuneId + '.canvas', tuneId + '.score', false, LINK_TO_LOCAL_JSON_FILES, DIAGNOSTICS);
+		} else {
+			var canvasDiv = document.getElementById(tuneId + '.canvas');
+			canvasDiv.innerHTML = '<p> ' + jsonFileName + '</p>';
 		}
 	}
     if (DIAGNOSTICS) {
@@ -29,11 +33,11 @@ function init() {
         //    var tuneCode = window.location.hostname + window.location.pathname + '/' + containerElements[i].id;
         //    containerElements[i].innerHTML = '<p> ' + tuneCode + '</p>';
         //}
-        var canvasElements = document.getElementsByClassName('divAbcTuneGraphCanvas');
-        for (var i = 0; i < canvasElements.length; ++i) {
-            var tuneCode = window.location.hostname + window.location.pathname + '/' + canvasElements[i].id;
-            canvasElements[i].innerHTML = '<p> ' + tuneCode + '</p>';
-        }
+        //var canvasElements = document.getElementsByClassName('divAbcTuneGraphCanvas');
+        //for (var i = 0; i < canvasElements.length; ++i) {
+        //    var tuneCode = window.location.hostname + window.location.pathname + '/' + canvasElements[i].id;
+        //    canvasElements[i].innerHTML = '<p> ' + tuneCode + '</p>';
+        //}
         var scoreElements = document.getElementsByClassName('divAbcTuneGraphScore');
         for (var i = 0; i < scoreElements.length; ++i) {
             var tuneCode = window.location.hostname + window.location.pathname + '/' + scoreElements[i].id;
